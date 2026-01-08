28127b740525b7b435df976832ce347c12e1c3ab
Samuel Leathers <samuel.leathers@iohk.io>
2026-01-08 13:33:44 -0500

    rust: more robust packages.nix example

--- rust/perSystem/packages.nix
+++ rust/perSystem/packages.nix
@@ -7,6 +7,36 @@
     ...
   }: {
     packages = {
+      example = let
+        naersk-lib = inputs.naersk.lib.${system};
+      in
+        naersk-lib.buildPackage rec {
+          pname = "example";
+
+          src = with lib.fileset;
+            toSource {
+              root = ./..;
+              fileset = unions [
+                ../Cargo.lock
+                ../Cargo.toml
+                ../src
+              ];
+            };
+
+          buildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          meta = {
+            mainProgram = pname;
+            maintainers = with lib.maintainers; [
+              disassembler
+            ];
+            license = with lib.licenses; [
+              asl20
+            ];
+          };
+        };
     };
   };
 }
